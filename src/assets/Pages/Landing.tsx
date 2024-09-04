e76d254c7741d7697be2f9d96c0196683334dcfb
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-04 12:17:12 -0400

    chore: Update landing page layout and add new components

---
+++ src/assets/Pages/Landing.tsx
@@ -0,0 +1,31 @@
+import MainNavbar from "../Components/MainNavbar";
+
+export const Landing = () => {
+  return (
+    <div>
+      <MainNavbar />
+      <div className="landing-info flex flex-col-reverse lg:flex-row my-32">
+        <div className="personal-intro flex flex-col">
+          <p className="text-darkmode-text font-opensans font-light text-center">
+            Hi, my name is
+          </p>
+          <h1 className="text-darkmode-text font-semibold lg:text-[64px] text-[32px] font-opensans text-center">
+            Jose E. Payamps
+          </h1>
+          <h2 className="text-darkmode-text font-semibold font-opensans text-center">
+            I am a <span className="text-darkmode-accent">Software Developer</span>
+          </h2>
+          <p className="text-darkmode-text font-light font-opensans pt-[20px] text-center">
+            I thrive on crafting software solutions from ideation to execution.
+            From envisioning innovative concepts to building user-focused
+            products, I use the latest technologies to deliver impactful
+            solutions that create a better user experience.
+          </p>
+        </div>
+        <div className="head-shot mask mask-hexagon bg-gray-900 ">
+          <img className="px-12" src="jose.png" alt="jose-headshot" />
+        </div>
+      </div>
+    </div>
+  );
+};
