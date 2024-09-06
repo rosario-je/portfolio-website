23a1a4673f9aa9d7fec1dd596c239ea2c9c28eb9
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-05 23:34:23 -0400

    refactor: Update Projects page layout and spacing

--- src/assets/Pages/Projects.tsx
+++ src/assets/Pages/Projects.tsx
@@ -4,7 +4,7 @@ export const Projects = () => {
   return (
     <section className="flex flex-col justify-center items center">
       <h1 className="font-opensans font-bold text-center text-darkmode-light-blue text-3xl pb-10">Some projects i have worked on:</h1>
-      <div className="projects flex flex-col gap-y-60">
+      <div className="projects flex flex-col gap-y-48">
         <ProjectCard
         // id={project.id}
         // project={project}
