f2b39b8394068e0ba4c4b4b7404cb4319b31d594
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-05 22:53:32 -0400

    refactor: Update App component to include MainNavbar, About, and Skills pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import MainNavbar from "./assets/Components/MainNavbar";
+import { About } from "./assets/Pages/About";
 import { Landing } from "./assets/Pages/Landing";
+import { Skills } from "./assets/Pages/Skills";
 
 function App() {
   return (
-    <div className="bg-slate-700 h-screen">
-      <div className="xl:mx-[300px] md:mx-[150px] sm:mx-[75px] mx-[20px] bg-darkmode-background h-screen">
+    <div className="bg-slate-700">
+      <div className="xl:mx-[300px] md:mx-[150px] sm:mx-[75px] mx-[20px] bg-darkmode-background">
+        <MainNavbar />
         <Landing />
+        <About />
+        <Skills />
       </div>
     </div>
   );
