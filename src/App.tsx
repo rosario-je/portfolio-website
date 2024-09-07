3cab50f3743511030626ffe753323479d68a8da0
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-06 22:21:36 -0400

    Refactor App component to add Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import "./App.css";
 import MainNavbar from "./assets/Components/MainNavbar";
 import { About } from "./assets/Pages/About";
+import { Contact } from "./assets/Pages/Contact";
 import { Landing } from "./assets/Pages/Landing";
 import { Projects } from "./assets/Pages/Projects";
 import { Skills } from "./assets/Pages/Skills";
 
 function App() {
   return (
-    <div className="bg-slate-700">
+    <div className="bg-darkmode-background">
       <div className="xl:mx-[300px] md:mx-[150px] sm:mx-[75px] mx-[20px] bg-darkmode-background">
         <MainNavbar />
         <Landing />
         <About />
         <Skills />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
