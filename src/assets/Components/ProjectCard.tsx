feeb28525de58432349e82c876a9c0ff29ae3639
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-08 18:24:50 -0400

    Refactor ProjectCard component to add GitHub repository link

--- src/assets/Components/ProjectCard.tsx
+++ src/assets/Components/ProjectCard.tsx
@@ -1,23 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faGithub);
 
 interface ProjectCardProps {
   project: {
-    id: number,
+    id: number;
     projectName: string;
     projectSubtitle: string;
     projectDesc: string;
     techStack: Array<string>;
-    imgUrl: string
+    imgUrl: string;
+    projectRepo: string;
   };
 }
 
 export const ProjectCard: React.FC<ProjectCardProps> = (props) => {
   return (
     <div className="project-card flex flex-col justify-center items-center">
-      <div className="left-side shadow-[_10px_10px_0px_rgba(15,135,194)]">
-        <img className="object-contain h-40" src={props.project.imgUrl} alt="project-img" />
+      <div
+        className={`left-side ${
+          props.project.id % 2 == 0
+            ? "shadow-[_10px_10px_0px_rgba(15,135,194)]"
+            : "shadow-[_-10px_10px_0px_rgba(15,135,194)]"
+        }`}
+      >
+        <img
+          className="object-contain h-40"
+          src={props.project.imgUrl}
+          alt="project-img"
+        />
       </div>
       <div className="right-side flex flex-col py-4">
-        <h1 className="text-center font-opensans font-bold text-7xl">{props.project.id}</h1>
+        <h1 className="text-center font-opensans font-bold text-7xl">
+          {props.project.id}
+        </h1>
         <h2 className="text-center font-opensans font-bold text-4xl text-darkmode-light-blue">
           {props.project.projectName}
         </h2>
@@ -26,14 +43,21 @@ export const ProjectCard: React.FC<ProjectCardProps> = (props) => {
         </h3>
         <p className="font-opensans font-light ">{props.project.projectDesc}</p>
       </div>
-      <div className="tech-stack pt-3 flex flex-wrap justify-center gap-x-5  border-t-[1px] border-t-darkmode-primary">
-        {props.project.techStack.map((tech) => {
-          return (
-            <span key={tech}>{tech}</span>
-          )
-        })}
-        <span>Github Logo</span>
+      <div className="tech-stack pt-3 flex flex-wrap justify-center gap-x-5  border-t-[1px] border-t-darkmode-primary flex-col items-center">
+        <div className="flex flex-row gap-x-5 pb-5">
+          {props.project.techStack.map((tech) => {
+            return <span key={tech}>{tech}</span>;
+          })}
+        </div>
+        <a href={props.project.projectRepo} target="_blank" className="transition ease-in-out delay-20 hover:shadow-[0px_0px_62px_0px_#4a5568]">
+          <FontAwesomeIcon
+            className="text-4xl"
+            icon={faGithub}
+            style={{ color: "#FFFFFF" }}
+          />
+        </a>
       </div>
     </div>
   );
 };
+
