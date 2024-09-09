d454483c3147f032774e2ba610ef41cd466a196c
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-09 01:34:24 -0400

    Refactor project images in ProjectsObjects.tsx

--- src/assets/imports/ProjectsObjects.tsx
+++ src/assets/imports/ProjectsObjects.tsx
@@ -5,7 +5,7 @@ export const projectsArray = [
     projectSubtitle: "Collaborative coding platform",
     projectDesc: "Colab is a collaborative platform that allows developers to create, join, and manage programming projects. Users can discover projects, collaborate with team members, and effectively manage project details. The app features user authentication, real-time chat, and a project dependant links for GitHub, Trello, and Figma to streamline the project management process.",
     techStack: ['ReactJS', 'Express', 'NodeJS', 'PostgreSQL', 'SocketIO'],
-    imgUrl: '/project_pics/PlotTwist_feed.png',
+    imgUrl: '/project_pics/CoLab_Feed_page.jpg',
     projectRepo: 'https://github.com/rosario-je/CoLab'
   },
   {
@@ -23,7 +23,7 @@ export const projectsArray = [
     projectSubtitle: "Interactive photo exploration app",
     projectDesc: "PhotoLabs is a React-based single-page application (SPA) that provides a dynamic user experience for browsing and interacting with photos. Users can explore various photo categories, view larger versions of photos, and like their favorites, with a notification icon indicating liked photos. Developed as a part of the Lighthouse Labs Web Development Bootcamp, the app uses React components, hooks, and state management, with data fetched and persisted via an API. Technologies used include React, SASS, Express, and PostgreSQL, offering a seamless interface for photo enthusiasts to explore and enjoy diverse images.",
     techStack: ['ReactJS', 'HTML', 'SASS', 'ExpressJS', 'PostgreSQL'],
-    imgUrl: '/project_pics/PlotTwist_feed.png',
+    imgUrl: '/project_pics/photo-labs-home.png',
     projectRepo: 'https://github.com/rosario-je/Photo-Labs'
   },
   {
@@ -32,7 +32,7 @@ export const projectsArray = [
     projectSubtitle: "Secure URL shortening tool",
     projectDesc: "TinyApp is a simple and secure URL shortening web application built with Node.js and Express.js. It allows users to create and manage shortened URLs, providing easy redirection to original long links. The app features user registration and authentication with secure password hashing, URL management for editing and deleting shortened URLs, and a streamlined user interface with EJS templates. TinyApp is ideal for anyone looking to organize and share URLs efficiently.",
     techStack: ['EJS', 'CSS', 'ExpressJS', 'NodeJS'],
-    imgUrl: '/project_pics/PlotTwist_feed.png',
+    imgUrl: '/project_pics/TinyApp_feed.jpg',
     projectRepo: 'https://github.com/rosario-je/tinyapp-'
   },
 ]
