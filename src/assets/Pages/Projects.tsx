666b09628423af57822f4baa3078764e9b68227f
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-09 12:41:50 -0400

    Refactor Projects page layout and styling

--- src/assets/Pages/Projects.tsx
+++ src/assets/Pages/Projects.tsx
@@ -3,11 +3,14 @@ import { projectsArray } from "../imports/ProjectsObjects";
 
 export const Projects = () => {
   return (
-    <section className="flex flex-col justify-center items center min-h-screen" id="work">
+    <section
+      className="flex flex-col justify-center items-center min-h-screen px-4 sm:px-6 lg:px-8 pb-20"
+      id="work"
+    >
       <h1 className="font-opensans font-bold text-center text-darkmode-light-blue text-3xl pb-10">
-        Some projects i have worked on:
+        Some projects I have worked on:
       </h1>
-      <div className="projects flex flex-col gap-y-48 lg:pt-60 lg:gap-y-80">
+      <div className="projects flex flex-col gap-y-20 lg:pt-20 lg:gap-y-40">
         {projectsArray.map((project) => {
           return <ProjectCard key={project.id} project={project} />;
         })}
