6b47e068301dbb08bd69d685f3cb2a00638e429b
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-09 01:34:28 -0400

    Refactor About page to include responsive layout and images

--- src/assets/Pages/About.tsx
+++ src/assets/Pages/About.tsx
@@ -8,12 +8,12 @@ library.add(faGithub, faLinkedin, faEnvelope);
 export const About = () => {
   return (
     <div>
-      <div className="about-info flex flex-col justify-center">
+      <div className="about-info flex flex-col justify-center lg:h-screen">
         <div className="about-info-details">
-          <h2 className="text-darkmode-accent font-bold font-opensans text-center text-[40px] ">
+          <h2 className="text-darkmode-accent font-bold font-opensans text-center text-[40px] lg:text-start">
             About me
           </h2>
-          <p className="text-darkmode-text font-light font-opensans text-center">
+          <p className="text-darkmode-text font-light font-opensans text-center lg:text-start lg:w-3/4 lg:text-2xl">
             I’m a recent Lighthouse Labs Web Flex Program graduate transitioning
             from Architecture to tech. Passionate about using the latest
             technologies to bring ideas to life, I’m dedicated to delivering
@@ -21,7 +21,7 @@ export const About = () => {
             continuously learn and improve, I thrive on new challenges and
             opportunities to enhance my skills as a developer.
           </p>
-          <div className="flex flex-row justify-between items-center py-3">
+          <div className="flex flex-row justify-between items-center py-3 lg:justify-start lg:gap-x-20">
             <a
               href="https://drive.google.com/file/d/1479NLcaFTsl6MkmnyxPF0rasCC2DlSH_/view?usp=sharing"
               target="_blank"
@@ -53,19 +53,19 @@ export const About = () => {
             </a>
           </div>
         </div>
-        <div className="about-info-imgs flex flex-col gap-y-5 items-center pt-5">
+        <div className="about-info-imgs flex flex-col gap-y-5 items-center pt-5 lg:flex-row lg:justify-between">
           <img
-            className="w-56 border-2 border-darkmode-secondary rounded-3xl"
+            className="w-56 border-2 border-darkmode-secondary rounded-3xl lg:w-1/5"
             src="/about/coding-buddy.png"
             alt="bobby coding"
           />
           <img
-            className="border-2 border-darkmode-secondary rounded-3xl"
+            className="border-2 border-darkmode-secondary rounded-3xl lg:w-1/3"
             src="/about/jose-coding.png"
             alt="jose coding during bootcamp"
           />
           <img
-            className="w-56 border-2 border-darkmode-secondary rounded-3xl"
+            className="w-56 border-2 border-darkmode-secondary rounded-3xl lg:w-1/5"
             src="/about/coding-cafe.png"
             alt="coding at a bubble tea place"
           />
