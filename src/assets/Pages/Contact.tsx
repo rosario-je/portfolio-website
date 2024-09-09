304ae478a384db384f5394a8019c0a9f264dacd5
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-09 01:34:33 -0400

    Refactor Contact page to include responsive layout and links

--- src/assets/Pages/Contact.tsx
+++ src/assets/Pages/Contact.tsx
@@ -8,11 +8,11 @@ library.add(faGithub, faLinkedin, faEnvelope);
 export const Contact = () => {
   return (
     <section className="flex flex-col justify-center items center h-screen">
-      <h1 className="font-opensans font-bold text-center text-darkmode-light-blue text-3xl pb-10">
+      <h1 className="font-opensans font-bold text-center text-darkmode-light-blue text-3xl pb-10 lg:text-left">
         Lets Connect!
       </h1>
-      <div className="links flex flex-col items-center gap-y-5">
-        <a href="https://www.linkedin.com/in/joseeduardopayamps/" target="_blank">
+      <div className="links flex flex-col items-center gap-y-5 lg:flex-row lg:justify-between">
+        <a href="https://www.linkedin.com/in/joseeduardopayamps/" target="_blank" className="transition ease-in-out delay-150 hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]">
           <div className="w-48 h-48 bg-darkmode-secondary flex justify-center items-center">
             <FontAwesomeIcon
               className="text-4xl"
@@ -21,7 +21,7 @@ export const Contact = () => {
             />
           </div>
         </a>
-        <a href="https://github.com/rosario-je/" target="_blank">
+        <a href="https://github.com/rosario-je/" target="_blank" className="transition ease-in-out delay-150 hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]">
           <div className="w-48 h-48 bg-darkmode-secondary flex justify-center items-center">
             <FontAwesomeIcon
               className="text-4xl"
@@ -30,7 +30,7 @@ export const Contact = () => {
             />
           </div>
         </a>
-        <a href="mailto:jose.payampsrosario@outlook.com" target="_blank">
+        <a href="mailto:jose.payampsrosario@outlook.com" target="_blank" className="transition ease-in-out delay-150 hover:shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]">
           <div className="w-48 h-48 bg-darkmode-secondary flex justify-center items-center">
             <FontAwesomeIcon
               className="text-4xl"
