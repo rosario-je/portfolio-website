653956c16c78a18ca727ce4e10cc3ac7b992b4aa
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-09-10 14:09:16 -0400

    Refactor App component to include responsive layout and fix text alignment

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import { Skills } from "./assets/Pages/Skills";
 
 function App() {
   return (
-    <div className="bg-darkmode-background">
-      <div className="xl:mx-[300px] md:mx-[150px] sm:mx-[75px] mx-[20px] bg-darkmode-background scroll-smooth">
+    <div className="bg-darkmode-background overflow-x-hidden">
+      <div className="2xl:mx-[300px] xl:mx-[200px] md:mx-[150px] sm:mx-[75px] mx-[20px] bg-darkmode-background scroll-smooth">
         <MainNavbar />
         <Landing />
         <About />
